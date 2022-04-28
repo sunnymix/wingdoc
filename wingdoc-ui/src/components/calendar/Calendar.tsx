06cce3c08bf8b92a8bec780db04686bc37e80495
sunnymix <sunny@sunnymix.com>
2022-04-29 03:20:59 +0800

    添加日历视图：天、周、月

--- wingdoc-ui/src/components/calendar/Calendar.tsx
+++ wingdoc-ui/src/components/calendar/Calendar.tsx
@@ -1,7 +1,14 @@
 import { forwardRef } from "react";
+import { Radio } from "antd";
 
 export interface CalendarProps {}
 
 export default forwardRef((props: CalendarProps, ref) => {
-  return <>Calendar</>
+  return <>
+  <Radio.Group defaultValue="week" buttonStyle="solid">
+    <Radio.Button value="day">Day</Radio.Button>
+    <Radio.Button value="week">Week</Radio.Button>
+    <Radio.Button value="month">Month</Radio.Button>
+  </Radio.Group>
+  </>
 });

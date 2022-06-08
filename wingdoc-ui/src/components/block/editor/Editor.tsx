8620182b81dc00e10bcfb95bf6d50ad651f3147a
sunnymix <sunny@sunnymix.com>
2022-06-09 00:36:32 +0800

    编辑器：创建 EditorHook

--- wingdoc-ui/src/components/block/editor/Editor.tsx
+++ wingdoc-ui/src/components/block/editor/Editor.tsx
@@ -1,25 +1,38 @@
-import { forwardRef, useState } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import { BlockProps } from "../Block";
 import './EditorStyle.css';
+import { useEditor } from "./EditorHook";
+import { Input } from "antd";
+const { TextArea } = Input;
+import { ArrowUpOutlined, ArrowDownOutlined, LinkOutlined } from '@ant-design/icons';
 
 export default forwardRef((props: BlockProps, ref) => {
 
-  const [text, setText] = useState<string>(props.data.text || '');
+  const { editorProps, editorText, clearEditor } = useEditor(props.data.text);
+
+  useEffect(() => {
+  }, [editorText]);
 
   return (
+  <>
   <div className='editor'>
-    <div className='editor_indicator'></div>
     <div className='editor_side'>
-      <button className='editor_sidebar'>
-        <span className="editor_sidebar_icon"></span>
+      <button className='editor_sidebar btn'>
+        <span className='btn_border_icon'></span>
       </button>
+      <div className="editor_controls">
+        <button className='btn'><LinkOutlined /></button>
+        <button className='btn'><ArrowUpOutlined /></button>
+        <button className='btn'><ArrowDownOutlined /></button>
+      </div>
     </div>
     <div className='editor_body'>
-      <div
-        className='editor_content'
-        contentEditable='plaintext-only'
-        suppressContentEditableWarning={true}>{text}</div>
+      <div className='editor_content' {...editorProps} />
     </div>
   </div>
+  <div>
+    {/* <TextArea value={editorText} autoSize></TextArea> */}
+  </div>
+  </>
   );
 });
---
+++ wingdoc-ui/src/components/block/editor/EditorHook.ts
@@ -0,0 +1,65 @@
+import { useCallback, useMemo, useRef, useState } from "react"
+
+export const useEditor = (initialText: string) => {
+  
+  const [text, setText] = useState<string>(initialText);
+
+  const editorRef = useRef<any>(null);
+
+  const handleInput = useCallback((e) => {
+    let text = '';
+
+    e.target.childNodes.forEach((node: any, i: number) => {
+      text += (node.innerText || node.nodeValue || '').replace(/\n/g, '');
+      if (i != e.target.childNodes.length - 1) {
+        text += '\n';
+      }
+    });
+
+    if (text === '') {
+      editorRef.current.innerHTML = '';
+    } else {
+      setText(text);
+    }
+
+  }, []);
+
+  const handlePaste = useCallback((e) => {
+    e.preventDefault();
+    document.execCommand('insertText', false, e.clipboardData.getData('text'));
+  }, []);
+
+  const clearEditor = useCallback(() => {
+    setText('');
+    editorRef.current.innerHTML = '';
+  }, []);
+
+  const text2HTML = useCallback((text: string): string => {
+    return text
+      .split('\n')
+      .map((line: string) => {
+        const HTML = line
+          .replace(/&/gi, '&amp;')
+          .replace(/</gi, '&lt;')
+          .replace(' ', '&nbsp;');
+        return `<div>${HTML || '<br />'}</div>`;
+      }).join('');
+  }, []);
+
+  const editorProps = useMemo(() => ({
+    contentEditable: true,
+    onInput: handleInput,
+    onPaste: handlePaste,
+    ref: editorRef,
+    dangerouslySetInnerHTML: {
+      __html: text2HTML(initialText)
+    }
+  }), [initialText, handleInput, handlePaste]);
+
+  return {
+    editorProps,
+    editorText: text,
+    editorRef,
+    clearEditor,
+  }
+};

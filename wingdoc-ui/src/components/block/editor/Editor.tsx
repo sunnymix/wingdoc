6d20ca19d3949f9fcca76675360eeca477fb03e3
sunnymix <sunny@sunnymix.com>
2022-06-09 20:47:50 +0800

    编辑器：添加 controlsOpen 状态

--- wingdoc-ui/src/components/block/editor/Editor.tsx
+++ wingdoc-ui/src/components/block/editor/Editor.tsx
@@ -10,17 +10,23 @@ export default forwardRef((props: BlockProps, ref) => {
 
   const { editorProps, editorText, clearEditor } = useEditor(props.data.text);
 
+  // --- editorText:
+
   useEffect(() => {
   }, [editorText]);
+
+  // --- sidebar:
+
+  const [controlsOpen, setControlsOpen] = useState<boolean>(false);
 
   return (
   <>
   <div className='editor'>
     <div className='editor_side'>
-      <button className='editor_sidebar btn'>
+      <button className='editor_sidebar btn' onMouseMove={() => setControlsOpen(true)} onMouseLeave={() => setControlsOpen(false)}>
         <span className='btn_border_icon'></span>
       </button>
-      <div className="editor_controls">
+      <div className={`editor_controls ${controlsOpen ? 'open' : ''}`} onMouseMove={() => setControlsOpen(true)} onMouseLeave={() => setControlsOpen(false)}>
         <button className='btn'><LinkOutlined /></button>
         <button className='btn'><ArrowUpOutlined /></button>
         <button className='btn'><ArrowDownOutlined /></button>

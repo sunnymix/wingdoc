901c1a3dde7eb36c8e566f0c8c84f0dfef22e31d
sunnymix <sunny@sunnymix.com>
2022-06-15 03:31:14 +0800

    清理代码

--- wingdoc-ui/src/pages/index.tsx
+++ wingdoc-ui/src/pages/index.tsx
@@ -1,13 +1,10 @@
 import Nav from '@/components/common/Nav';
 import styles from './index.less';
 import TaskTable from '@/components/task/TaskTable';
-import Text from '@/components/common/text/Text';
 
 export default () => {
   return (
   <>
-    <Text value='Human beings ... are very much at the mercy of the particular language which has become the medium of expression for their society. It is quite an illusion to imagine that one adjusts to reality essentially without the use of language and that language is merely an incidental means of solving specific problems of communication and reflection. The fact of the matter is that the “real world” is to a large extent unconsciously built up on the language habits of the group.' />
-    <br />
     <TaskTable />
   </>);
 };

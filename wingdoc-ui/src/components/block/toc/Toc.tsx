bb07eb205c4a2a869e36590982c45c802dd50ce3
sunnymix <sunny@sunnymix.com>
2022-07-24 10:22:26 +0800

    TOC：使用umi框架的页面跳转功能

--- wingdoc-ui/src/components/block/toc/Toc.tsx
+++ wingdoc-ui/src/components/block/toc/Toc.tsx
@@ -2,6 +2,7 @@ import { forwardRef, useEffect, useState } from 'react';
 import { BlockType } from '../block/Block';
 import './TocStyle.css';
 import { Link } from "umi";
+import { history } from "umi";
 
 export interface TocProps {
   docId: string,
@@ -9,6 +10,8 @@ export interface TocProps {
 };
 
 export default forwardRef((props: TocProps, ref) => {
+
+  // --- items:
 
   const [items, setItems] = useState<any[]>([]);
 
@@ -19,17 +22,26 @@ export default forwardRef((props: TocProps, ref) => {
           id: block.id,
           type: block.type,
           text: block.text,
+          link: `/doc/${props.docId}?block=${block.id}`,
         };
       }
     }).filter(item => typeof(item) != 'undefined');
-
     setItems(newItems);
   }, [props.blocks]);
+
+  // --- click:
+  
+  const handleClick = (e: any, item: any) => {
+    e.preventDefault();
+    history.push(item.link);
+  };
+
+  // --- ui:
 
   return (
     <div className='toc'>
       {items.map((item: any, index: number) =>
-        <a className='toc_item' key={item.id} href={`/wingdoc-ui/doc/${props.docId}?block=${item.id}`}>
+        <a className='toc_item' key={item.id} href={item.link} onClick={(e: any) => handleClick(e, item)}>
           {item.text}
         </a>
       )}

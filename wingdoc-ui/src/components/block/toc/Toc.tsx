89fa6dbfc7b4c2b1c61f7295bdf29ec35e30d934
sunnymix <sunny@sunnymix.com>
2022-08-04 12:55:49 +0800

    优化目录中任务文本的截取方式

--- wingdoc-ui/src/components/block/toc/Toc.tsx
+++ wingdoc-ui/src/components/block/toc/Toc.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useEffect, useState } from 'react';
+import { forwardRef, useCallback, useEffect, useState } from 'react';
 import { BlockType } from '../block/Block';
 import './TocStyle.css';
 import { Link } from "umi";
@@ -15,13 +15,26 @@ export default forwardRef((props: TocProps, ref) => {
 
   const [items, setItems] = useState<any[]>([]);
 
+  const prettyText = useCallback((block: any) => {
+    let text = block.text || '';
+    if (block.type == BlockType.TASK) {
+      const lineBreakIndex = text.indexOf('\n');
+      if (lineBreakIndex > 0) {
+        text = text.substring(0, lineBreakIndex);
+      }
+    } else {
+      text = text.substr(0, 20);
+    }
+    return text;
+  }, []);
+
   useEffect(() => {
     const newItems = props.blocks.map((block: any, index: number) => {
       if (BlockType.tocFilter(block.type)) {
         return { 
           id: block.id,
           type: block.type,
-          text: (block.text || '').substr(0, 20),
+          text:  prettyText(block),
           link: `/doc/${props.docId}?block=${block.id}`,
         };
       }
